d9c18ea0b2fa3c78f4514d779d75f8961235c5a0
maurypereyra <maury.pereyra@gmail.com>
2017-01-24 15:18:22 -0300

    title: "Dijkstra: Shortest Reach 2" link: "https://www.hackerrank.com/challenges/dijkstrashortreach?h_r=next-challenge&h_v=zen"

---
+++ src/com/hacker_rank_solutions/Solution.java
@@ -0,0 +1,27 @@
+package com.hacker_rank_solutions;
+
+import java.util.Scanner;
+
+public class Solution {
+    private static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        dijkstra();
+    }
+
+    private static void dijkstra() {
+        int T, N, M, x, y, r;
+        T = in.nextInt();
+        //each test
+        for (int i = 0; i < T; i++) {
+            in.nextLine();
+            N = in.nextInt();
+            M = in.nextInt();
+
+            for (int j = 0; j < M; j++) {
+
+            }
+        }
+    }
+}
